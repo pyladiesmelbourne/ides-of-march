d80c1d916cf399fd726a2b492952185c29013e8a
Maree Carroll <maree.carroll@gmail.com>
2019-03-10 21:50:19 +1100

    feat: add simple plotting python as would be used in vs code with neuron

---
+++ simple-plotting-neuron.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+PyLadies: IDEs of March (March 2019 PyLadies Melbourne).
+
+Jake Vanderplas' Python Data Science Handbook
+Notebook on Simple Line Plots in Spyder
+"""
+
+# https://github.com/jakevdp/PythonDataScienceHandbook/blob/master/notebooks/04.01-Simple-Line-Plots.ipynb
+
+# install the neuron extension
+# click the icon to open output pane (Ctrl+K Ctrl+V)
+# select code and alt-enter to run in output pane
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
+fig = plt.figure()
+ax = plt.axes()
+
+x = np.linspace(0, 10, 1000)
+plt.plot(x, np.sin(x))
+
+plt.plot(x, np.sin(x))
+plt.plot(x, np.cos(x))
+
+plt.plot(x, np.sin(x - 0), color='blue')        # specify color by name
+plt.plot(x, np.sin(x - 1), color='g')           # short color code (rgbcmyk)
+plt.plot(x, np.sin(x - 2), color='0.75')        # Grayscale between 0 and 1
+plt.plot(x, np.sin(x - 3), color='#FFDD44')     # Hex code (RRGGBB from 00 to FF)
+plt.plot(x, np.sin(x - 4), color=(1.0,0.2,0.3)) # RGB tuple, values 0 to 1
+plt.plot(x, np.sin(x - 5), color='chartreuse') # all HTML color names supported
+
+plt.plot(x, x + 0, linestyle='solid')
+plt.plot(x, x + 1, linestyle='dashed')
+plt.plot(x, x + 2, linestyle='dashdot')
+plt.plot(x, x + 3, linestyle='dotted')
+
+# For short, you can use the following codes:
+plt.plot(x, x + 4, linestyle='-')  # solid
+plt.plot(x, x + 5, linestyle='--') # dashed
+plt.plot(x, x + 6, linestyle='-.') # dashdot
+plt.plot(x, x + 7, linestyle=':')  # dotted
+
+plt.plot(x, x + 0, '-g')  # solid green
+plt.plot(x, x + 1, '--c') # dashed cyan
+plt.plot(x, x + 2, '-.k') # dashdot black
+plt.plot(x, x + 3, ':r')  # dotted red
+
+plt.plot(x, np.sin(x))
+plt.xlim(-1, 11)
+plt.ylim(-1.5, 1.5)
+
+plt.plot(x, np.sin(x))
+plt.xlim(10, 0)
+plt.ylim(1.2, -1.2)
+
+plt.plot(x, np.sin(x))
+plt.axis([-1, 11, -1.5, 1.5])
+
+plt.plot(x, np.sin(x))
+plt.axis('tight')
+
+plt.plot(x, np.sin(x))
+plt.axis('equal')
+
+
+plt.plot(x, np.sin(x))
+plt.title("A Sine Curve")
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+
+"""Add a legend"""
+plt.plot(x, np.sin(x), '-g', label='sin(x)')
+plt.plot(x, np.cos(x), ':b', label='cos(x)')
+plt.axis('equal')
+plt.legend()
+
+ax = plt.axes()
+ax.plot(x, np.sin(x))
+ax.set(xlim=(0, 10), ylim=(-2, 2),
+       xlabel='x', ylabel='sin(x)',
+       title='A Simple Plot')
+
